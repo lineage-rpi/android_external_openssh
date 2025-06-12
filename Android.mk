fe0e83b3605accef1e98e76e55877c696fd9ebff
Konsta <konsta09@gmail.com>
2025-06-12 21:23:43 +0300

    remove duplicate android sshd config

--- Android.mk
+++ Android.mk
@@ -329,7 +329,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := sshd_config
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/ssh
-LOCAL_SRC_FILES := sshd_config.android-sshd
+LOCAL_SRC_FILES := sshd_config.android
 include $(BUILD_PREBUILT)
 
 ###################### start-ssh ######################
